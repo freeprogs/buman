5c69ab89f1917c5759512eb6223bb31a7d10a8cc
Slava <freeprogs.feedback@yandex.ru>
2018-08-25 17:55:24 +1100

    Add a stub for ContextCommands.file_to_dir()

--- src_template/buman.py
+++ src_template/buman.py
@@ -708,7 +708,10 @@ class ContextCommands:
 
     def file_to_dir(self, src, dst):
         """."""
-        return CommandResult()
+        out = CommandResult()
+        out.flags |= CommandResult.F_OK
+        out.status = (0, 'Success')
+        return out
 
     def dir_to_dir(self, src, dst):
         """."""
